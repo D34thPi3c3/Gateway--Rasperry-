94f11b21a4fe7a48f5cbb5d11bdb28e54af01723
D34thPi3c3 <rafaeleberle1994@gmail.com>
2016-12-25 13:29:42 +0100

    ad code itoh

---
+++ C-Code/itoh.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+main(){
+	uint16_t a[8] = {672, 162, 1002, 580, 918, 187, 309, 465};
+	uint16_t send[5];
+	int count = 0;
+	
+	if(count == 0){
+		uint16_t zsave;
+		zsave = a[0]+(0b0000000000111111&(a[1]>>10))
+	}
+	
+	
+	switch(fourbit){
+		case 0: break;
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		
+		default:
+			printf("Sollte nicht möglich sein das wert über 15 ist.")
+			break;
+	}
+}
