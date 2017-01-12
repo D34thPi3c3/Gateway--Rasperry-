a878743349cb9876b627ad5718edabf5edfede3f
D34thPi3c3 <rafaeleberle1994@gmail.com>
2017-01-12 17:40:00 +0100

    v0.17
    common funktionier ändlich

--- single_chan_pkt_fwd-master/main.cpp
+++ single_chan_pkt_fwd-master/main.cpp
@@ -547,7 +547,7 @@ void receivepacket() {
 			
 			
 			FILE *f;
-			while(/*message[i]!='\0'*/(result[rhelp]!=0x611)&&(result[rhelp]!=1297)){
+			while((result[rhelp]!=1553)&&(result[rhelp]!=1297)){
 			
 			char hchar = message[i];
 			/*
@@ -643,7 +643,10 @@ void receivepacket() {
 				case 2:
 				hvar3 = mdez*1;
 				result[rhelp] = hvar1+hvar2+hvar3;
-				if((result[rhelp]==0x611)||(result[rhelp]==1297)){
+				if((result[rhelp]==1553)||(result[rhelp]==1297)){
+					if(result[rhelp]==1553){
+						result[rhelp] == 0;
+					}
 					rhelp--;
 				}
 				rhelp++;
@@ -678,7 +681,10 @@ void receivepacket() {
 				case 2:
 				hvar3 = mdez*1;
 				result[rhelp] = hvar1+hvar2+hvar3;
-				if((result[rhelp]==0x611)||(result[rhelp]==1297)){
+				if((result[rhelp]==1553)||(result[rhelp]==1297)){
+					if(result[rhelp]==1553){
+						result[rhelp] == 0;
+					}
 					rhelp--;
 				}
 				rhelp++;
@@ -700,10 +706,7 @@ void receivepacket() {
 			}
 			
 			//printf("Erste erfolge wurden erzielt: Das Resultat ist %d \n", result);
-			if(result[rhelp]!=0x611){
-				rhelp--;
-			}
-			
+						
 			if(result[rhelp]==1297){
 				printf("Juhu bis hierhin komme ich\n");
 				char buffer[200];
@@ -739,6 +742,8 @@ void receivepacket() {
             fflush(stdout);
 
         } // received a message
+		
+		
 
     } // dio0=1
 }
